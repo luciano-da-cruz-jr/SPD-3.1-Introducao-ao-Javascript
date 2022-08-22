4f99c36ac3ccd649a7d8244c796452c4bc6089b9
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-22 15:34:37 -0300

    Javascript em uma página web - Desativando botão ao chegar em -1 ou 10

--- 03 - Javascript em uma página web - Criando um contador com botões/assets/js/script.js
+++ 03 - Javascript em uma página web - Criando um contador com botões/assets/js/script.js
@@ -3,17 +3,36 @@ const elementAdd = document.getElementById("adicionar");
 var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0;
 
+
+
 elementSub.addEventListener("click",subtrair);
 elementAdd.addEventListener("click",adicionar)
 
 function adicionar()
-{
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+{   
+    elementSub.disabled = false;
+
+    if(currentNumber >= 0 && currentNumber <= 9)
+    {   
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }else
+    {
+        elementAdd.disabled = true;
+    }
+    
 }
 
 function subtrair()
 {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    elementAdd.disabled = false;
+
+    if(currentNumber >= 1 && currentNumber <= 10)
+    {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }else
+    {
+        elementSub.disabled = true;
+    }
 }
